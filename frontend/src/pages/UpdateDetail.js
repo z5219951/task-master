5a94a949fbf50225b08ffed4ab7e409f7b469877
Ka Wayne Ho <z5139681@unsw.edu.au>
2021-06-28 18:19:38 +1000

    Added error checking on update profile page + special case for password change

--- frontend/src/pages/UpdateDetail.js
+++ frontend/src/pages/UpdateDetail.js
@@ -10,6 +10,11 @@ const UpdateDetail = (props) => {
   const [detailProp, setDetailProp] = useState('');
   const [updateDetAlert, setUpdateDetAlert] = useState('')
   const [user, setUser] = useState({})
+  const [passwordBool, setPasswordBool] = useState(false);
+  const [oldPassword, setOldPassword] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [passwordAlert, setPasswordAlert] = useState('');
 
   // Find current user
   const currentUser = store.getState().id;
@@ -19,7 +24,6 @@ const UpdateDetail = (props) => {
     for (let i=0; i < users.length; i++) {
       // If user ID in database matches current user
       if (String(users[i].id) === String(currentUser)) {
-        console.log(users[i])
         setUser(users[i])
       }
     }
@@ -28,33 +32,127 @@ const UpdateDetail = (props) => {
     })
   },[props])
 
+  function verifyInfo (detailProp, detail) {
+    // Handles input for email field
+    setUpdateDetAlert('')
+    if (detailProp === 'email') {
+      const testEmail = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
+      if (!testEmail.test(detail)) {
+        setUpdateDetAlert('Please enter a valid email!')
+        return false;
+      }
+    }
+
+    if (detailProp === 'userName') {
+      if (detail === '') {
+        setUpdateDetAlert('Please enter a username!')
+        return false;
+      }
+    }
+
+    if (detailProp === 'firstName') {
+      const testWord = /^(\w+){1,30}$/
+      if (detail === '') {
+        setUpdateDetAlert('Please enter a valid first name!')
+        return false;
+      }
+    }
+
+    if (detailProp === 'lastName') {
+      const testWord = /^(\w+){1,30}$/
+      if (detail === '') {
+        setUpdateDetAlert('Please enter a valid last name!')
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   function handleSubmit (detailProp, detail) {
+    if (!verifyInfo(detailProp, detail)) {
+      return;
+    }
     const updateDet = {...user}; // Copy user into updateDet
     updateDet[detailProp] = detail; // Change selected field
     setUser(updateDet); //Set user
   }
 
+  function handlePassword(oldPassword, newPassword, confirmPass) {
+    setUpdateDetAlert('')
+    console.log(user['passWord'])
+    if (oldPassword !== user['passWord']) {
+      setUpdateDetAlert('Current password incorrect!')
+      return;
+    }
+    const testPass = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,16}$/
+    if (!testPass.test(newPassword)){
+      setUpdateDetAlert('Please enter a valid password')
+      return;
+    }
+
+    if (newPassword !== confirmPass) {
+      setUpdateDetAlert('Password and confirm password fields do not match!')
+      return;
+    }
+
+    const updateDet = {...user}; // Copy user into updateDet
+    updateDet['passWord'] = newPassword; // Change selected field
+    setUser(updateDet); //Set user
+    setPasswordAlert('Password Changed!')
+  }
+
   // Triggered when 'user' is modified
   useEffect(() => {
-    console.log(user)
     if (user !== '') {
       axios.put(`http://localhost:5000/userInform/${currentUser}`, user)
     }
   }, [user])
+
+  useEffect(() => {
+    if (detailProp === 'passWord') {
+      setPasswordBool(true)
+    } else {
+      setPasswordBool(false)
+    }
+  }, [detailProp])
   
   return (
     <>
     <div class="padding">
       <br/>
-      <div><h2>Current {props.label}: </h2> <h4>{user[detailProp]}</h4> </div>
-      <br/>
-      <div><h2>New {props.label}: </h2></div>
-      <div class="col-md-3">
-        <input class="form-control input-sm" type="text" id="name" onChange={(e) => setDetail(e.target.value)}></input>
-        <font color="red">{updateDetAlert}</font>
-      </div>
-      <br/>
-      <button type="button" class="btn btn-primary" onClick={(e) => handleSubmit(detailProp, detail)}>Submit</button>
+      {passwordBool ? <div>
+        <div><h2>Please enter your current password: </h2></div>
+        <div class="col-md-3">
+          <input class="form-control input-sm" type="password" id="name" onChange={(e) => setOldPassword(e.target.value)}></input>
+        </div>
+        <div><h2>New {props.label}: </h2></div>
+        <div class="col-md-3">
+          <input class="form-control input-sm" type="password" id="name" onChange={(e) => setNewPassword(e.target.value)}></input>
+        </div>
+        <div><h2>Confirm New {props.label}: </h2></div>
+        <div class="col-md-3">
+          <input class="form-control input-sm" type="password" id="name" onChange={(e) => setConfirmPass(e.target.value)}></input>
+          <font color="red">{updateDetAlert}</font>
+        </div>
+        <br/>
+        <button type="button" class="btn btn-primary" onClick={(e) => handlePassword(oldPassword, newPassword, confirmPass)}>Submit</button>
+        <br />
+        <font color="green">{passwordAlert}</font>
+      </div> 
+      
+      : <div>
+        <div><h2>Current {props.label}: </h2> <h4>{user[detailProp]}</h4> </div>
+        <br/>
+        <div><h2>New {props.label}: </h2></div>
+        <div class="col-md-3">
+          <input class="form-control input-sm" type="text" id="name" onChange={(e) => setDetail(e.target.value)}></input>
+          <font color="red">{updateDetAlert}</font>
+        </div>
+        <br/>
+        <button type="button" class="btn btn-primary" onClick={(e) => handleSubmit(detailProp, detail)}>Submit</button>
+          </div>
+      }
     </div>
     </>
   )
