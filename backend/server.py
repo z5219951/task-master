fb9b89ee76ea7fdd42928c510484a4f422c3225d
Justin Pham <z5075823@unsw.edu.au>
2021-07-09 20:37:29 +1000

    merged tasks/update to main

--- backend/server.py
+++ backend/server.py
@@ -353,6 +353,84 @@ class Users(Resource):
     def get(self, owner):
         return json.dumps({'tasks': getTasks(owner)})
 
+
+# update task info
+update_task_payload = api.model('update info', {
+    "id": fields.String,
+    "owner": fields.String,
+    "title": fields.String,
+    "description": fields.String,
+    "creation_date": fields.String,
+    "deadline": fields.String,
+    # "labels": fields.String,
+    "current_state": fields.String,
+    "progress": fields.Integer,
+    "time_estimate": fields.Integer,
+    "difficulty": fields.String
+})
+
+@api.route('/tasks/update', methods=['PUT'])
+class Users(Resource):
+    @api.response(200, 'Successfully updated task')
+    @api.response(404, 'Not Found')
+    @api.doc(description="Updates a task given its id")
+    @api.expect(update_task_payload)
+    def put(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', required=True)
+        parser.add_argument('owner')
+        parser.add_argument('title')
+        parser.add_argument('description')
+        parser.add_argument('creation_date')
+        parser.add_argument('deadline')
+        # parser.add_argument('labels')
+        parser.add_argument('current_state')
+        parser.add_argument('progress')
+        parser.add_argument('time_estimate')
+        parser.add_argument('difficulty')
+        args = parser.parse_args()
+        # print(args)
+
+        id = args.id
+        owner = args.owner
+        title = args.title
+        description = args.description
+        creation_date = args.creation_date
+        deadline = args.deadline
+        # labels = args.labels
+        current_state = args.current_state
+        progress = args.progress
+        time_estimate = args.time_estimate
+        difficulty = args.difficulty
+
+        conn = sqlite3.connect('clickdown.db')
+        c = conn.cursor()
+
+        query = f"""
+                UPDATE  users
+                SET     owner = '{owner}',
+                        title = '{title}',
+                        description = '{description}',
+                        creation_date = '{creation_date}',
+                        deadline = '{deadline}',
+                        current_state = '{current_state}'
+                        progress = '{progress}'
+                        time_estimate = '{time_estimate}'
+                        difficulty = '{difficulty}'
+                WHERE   id = '{id}';
+                """
+        try:
+            c.execute(query)
+        except:
+            c.close()
+            conn.close()
+            return {'value': False}, 200
+        c.close()
+        conn.close()
+
+        return {'value': True}
+
+
 if __name__ == '__main__':
     # params = config()
     # conn = psycopg2.connect(**params)
