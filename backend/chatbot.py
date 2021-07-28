c8eb27b6149b1fa15bd435cd2094cfcca950bb39
NicholaiRank <z5115301@student.unsw.edu.au>
2021-07-29 00:59:54 +1000

    Accept Connection working

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -2,8 +2,6 @@ from friends import *
 from functions import *
 from datetime import *
 
-
-
 def parseIntent(intent, dfResponse, email, initMsg):
     #currently does not reflect in front end???
     if(intent == "AddTask"):
@@ -28,7 +26,37 @@ def parseIntent(intent, dfResponse, email, initMsg):
         response = {'fulfillment_text': "I need to check who wants to connect with you."}
         #Handle getting potential connection list
     elif(intent == "AcceptConnection"):
-        response = {'fulfillment_text': "I need to see if this person wants to connect with you and approve it."}
+        params = dfResponse.query_result.parameters
+        fullName = params.fields['person'].struct_value['name']
+        fullName = fullName.split(' ')
+        firstName = fullName[0]
+        lastName = ""
+        i = 1
+        while ( i < len(fullName)):
+            lastName += fullName[i] + " "
+            i += 1
+        lastName = lastName[:-1]
+        print(firstName + "   " + lastName)
+        owner = getOwner(email)
+        users = getUsersByFirstLastName(firstName , lastName)
+        print(len(users))
+        if (len(users) > 1):
+
+            # Return Error
+            response = {'fulfillment_text': "You have multiple requests named {} {}. Please try again or use the interface to confirm the request.".format(firstName, lastName)}
+            print(response)
+            return response
+        elif(len(users) == 0):
+            response = {'fulfillment_text': "No user named {} {} exists. Please try again.".format(firstName, lastName)}
+            print(response)
+            return response
+        if(friendRequestRemove(users[0][0], owner)):
+            friendListAdd(users[0][0] , owner)
+            response = {'fulfillment_text': "Accepted request for {}".format(firstName + " " + lastName)}
+            print(response)
+            return response,200
+        response = {'fulfillment_text': "Request failed, no request found for {}".format(firstName + " " + lastName)}
+        print(response)
         #Handle accepting a user's connection request
     elif(intent == "DeclineConnection"):
         params = dfResponse.query_result.parameters
@@ -49,6 +77,9 @@ def parseIntent(intent, dfResponse, email, initMsg):
 
             # Return Error
             response = {'fulfillment_text': "You have multiple requests named {} {}. Please try again or use the interface to delete the request.".format(firstName, lastName)}
+            return response
+        elif(len(users) == 0):
+            response = {'fulfillment_text': "No user named {} {} exists. Please try again.".format(firstName, lastName)}
             return response
         if (friendRequestRemove(users[0][0] , owner )):
             response = {'fulfillment_text': "Declined request for {}".format(firstName + " " + lastName)}
