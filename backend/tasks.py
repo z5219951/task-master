f76d4093255d880d3aa73684d720ea1f027d0f70
NicholaiRank <z5115301@student.unsw.edu.au>
2021-07-31 11:47:58 +1000

    branchtest

--- backend/tasks.py
+++ backend/tasks.py
@@ -15,6 +15,7 @@ bp = Blueprint('tasks', __name__, url_prefix='/tasks')
 api = Namespace("tasks", "Operations for tasks")
 
 # create task
+#Priority and difficulty?
 task_payload = api.model('task', {
     "owner": fields.Integer,
     "title": fields.String,
