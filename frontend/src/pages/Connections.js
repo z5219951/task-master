17e1668a2edaa5e0147d458a3509778cd1fbee48
Ka Wayne Ho <z5139681@unsw.edu.au>
2021-08-02 18:53:45 +1000

    Added 'no connected users' display on My Connections'

--- frontend/src/pages/Connections.js
+++ frontend/src/pages/Connections.js
@@ -26,6 +26,7 @@ const Connections = () => {
     <div className="padding">
       <h1>Users I am connected with:</h1>
       <br/>
+      {friends.length === 0 ? <h3>No connected users</h3> : ''}
       {friends.map((friend, index) => {
         return <div key={friend.requestedUser} className="card m-4"> <h3 className="m-2">{friend.name} ({friend.email}) - {busy[index]}% Workload {busy[index] > 99 ? '(User Overloaded)' : ''}</h3> <br/> <ViewProfileButton id={friend.requestedUser}></ViewProfileButton></div>
       })}
